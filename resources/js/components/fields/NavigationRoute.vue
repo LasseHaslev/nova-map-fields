06fc0a82915260b3dee51f126eca91458bef0a57
Kevin Midboe <kevin.midboe@gmail.com>
2021-09-06 16:15:37 +0200

    Transform navigation response before bubbling input latlng

--- resources/js/components/fields/NavigationRoute.vue
+++ resources/js/components/fields/NavigationRoute.vue
@@ -39,18 +39,24 @@ export default {
       const cords =
         "6.985908836799895,61.28541170467011;7.092996407603286,61.23027059410228;10.761970380502788,59.927108594277456";
       const URL = `https://api.mapbox.com/directions/v5/mapbox/driving/${cords}?geometries=geojson&access_token=pk.eyJ1Ijoia2V2aW5taWRib2UiLCJhIjoiY2pydWhlamQyMHJ2NTRhdGN1em5ndXVyMyJ9.Ejdo_3iuuGOD662Bh6es4w`;
-      fetch(URL)
+      return fetch(URL)
         .then((resp) => resp.json())
-        .then((response) => {
-          const { coordinates } = response.routes.geometry;
-          console.log("got coordinates:", coordinates);
-        });
+        .then((response) => response.routes[0].geometry)
+        .then(this.mapNavigationRoute);
     },
 
-    triggerChange(markers) {
+    mapNavigationRoute(coords) {
+      return coords.map(([lng, lat]) => {
+        return { lat, lng };
+      });
+    },
+
+    async triggerChange(markers) {
       console.log("forwarding markers:", markers);
-      this.queryNavigation(markers);
-      this.$emit("input", markers);
+      const navigationRouteCoords = await this.queryNavigation(markers);
+      console.log("got navigationRouteCoords:", navigationRouteCoords);
+
+      this.$emit("input", navigationRouteCoords);
     },
   },
 };
