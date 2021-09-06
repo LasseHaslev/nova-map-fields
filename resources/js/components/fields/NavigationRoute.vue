0debcb02a174252b6f861658bd596e50ae598983
Kevin Midboe <kevin.midboe@gmail.com>
2021-09-06 16:24:45 +0200

    Dynamic cords from markers

--- resources/js/components/fields/NavigationRoute.vue
+++ resources/js/components/fields/NavigationRoute.vue
@@ -36,8 +36,10 @@ export default {
 
   methods: {
     queryNavigation(markers) {
-      const cords =
-        "6.985908836799895,61.28541170467011;7.092996407603286,61.23027059410228;10.761970380502788,59.927108594277456";
+      const cords = markers
+        .map((marker) => `${marker.lng},${marker.lat};`)
+        .slice(0, -1);
+
       const URL = `https://api.mapbox.com/directions/v5/mapbox/driving/${cords}?geometries=geojson&access_token=pk.eyJ1Ijoia2V2aW5taWRib2UiLCJhIjoiY2pydWhlamQyMHJ2NTRhdGN1em5ndXVyMyJ9.Ejdo_3iuuGOD662Bh6es4w`;
       return fetch(URL)
         .then((resp) => resp.json())
@@ -52,6 +54,10 @@ export default {
     },
 
     async triggerChange(markers) {
+      if (markers.length === 0) {
+        return;
+      }
+
       console.log("forwarding markers:", markers);
       const navigationRouteCoords = await this.queryNavigation(markers);
       console.log("got navigationRouteCoords:", navigationRouteCoords);
